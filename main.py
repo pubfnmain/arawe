5f1b558aafcaf903d6b9610e0d937124fcab587c
unknown <timurshermamatov1@gmail.com>
2025-02-04 11:14:59 +0600

    timur

--- main.py
+++ main.py
@@ -8,28 +8,24 @@ from starlette.staticfiles import StaticFiles
 from starlette.endpoints import WebSocketEndpoint
 
 from server import Server
-import uvicorn
-import os
+
+
 async def index(request):
     return FileResponse("index.html")
 
 
 @asynccontextmanager
 async def lifespan(app):
-    task = create_task(Server.loop())  
+    task = create_task(Server.loop())
     yield
 
 
 app = Starlette(
     debug=True,
     routes=(
-        Route('/', index),  
-        Mount('/static', app=StaticFiles(directory="static")), 
-        WebSocketRoute('/', Server),
-        WebSocketRoute('/ws', Server)
+        Route('/', index),
+        Mount('/static', app=StaticFiles(directory="static")),
+        WebSocketRoute('/', Server)
     ),
     lifespan=lifespan
 )
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
